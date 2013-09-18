c2599de57358a39d9082c486f347ca7afec7ed7b
Brian Koropoff <brian.koropoff@isilon.com>
2013-09-17 20:23:00 -0700

    test:readwrite: add more tests around 0-byte writes

--- test/readwrite.py
+++ test/readwrite.py
@@ -37,6 +37,8 @@
 import pike.model
 import pike.smb2
 import pike.test
+import array
+import random
 
 class ReadWriteTest(pike.test.PikeTest):
     # Test that we can write to a file
@@ -83,15 +85,15 @@ class ReadWriteTest(pike.test.PikeTest):
         
         chan.close(file)
 
-    # Test that we can read back what we write
-    def test_read(self):
+    # Test that a 0-byte write succeeds
+    def test_write_none(self):
         chan, tree = self.tree_connect()
-        buffer = "testing123"
+        buffer = None
 
         share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
 
         file = chan.create(tree,
-                           'read.txt',
+                           'write.txt',
                            access=pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE,
                            share=share_all,
                            disposition=pike.smb2.FILE_SUPERSEDE,
@@ -101,12 +103,94 @@ class ReadWriteTest(pike.test.PikeTest):
         bytes_written = chan.write(file,
                                    0,
                                    buffer)
-        self.assertEqual(bytes_written, len(buffer))
+        self.assertEqual(bytes_written, 0)
+        
+        chan.close(file)
 
-        data = chan.read(file,
-                         len(buffer),
-                         0)
+    # Test that a 0-byte write triggers access checks
+    # (and fails since we do not have write access)
+    def test_write_none_access(self):
+        chan, tree = self.tree_connect()
 
-        self.assertEqual(buffer, data.tostring())
+        share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
+
+        file = chan.create(tree,
+                           'write.txt',
+                           access=pike.smb2.FILE_READ_DATA | pike.smb2.DELETE,
+                           share=share_all,
+                           disposition=pike.smb2.FILE_SUPERSEDE,
+                           oplock_level=pike.smb2.SMB2_OPLOCK_LEVEL_EXCLUSIVE).result()
+       
+        with self.assert_error(pike.smb2.STATUS_ACCESS_DENIED):
+            chan.write(file, 0, None)
+        
+        chan.close(file)
+
+    # Test that 0-byte write does not cause an oplock break
+    def test_write_none_oplock(self):
+        chan, tree = self.tree_connect()
+
+        share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
+
+        file = chan.create(tree,
+                           'write.txt',
+                           access=pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE,
+                           share=share_all,
+                           disposition=pike.smb2.FILE_SUPERSEDE,
+                           options=pike.smb2.FILE_DELETE_ON_CLOSE,
+                           oplock_level=pike.smb2.SMB2_OPLOCK_LEVEL_NONE).result()
+
+        file2 = chan.create(tree,
+                            'write.txt',
+                            access=pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE,
+                            share=share_all,
+                            disposition=pike.smb2.FILE_SUPERSEDE,
+                            options=pike.smb2.FILE_DELETE_ON_CLOSE,
+                            oplock_level=pike.smb2.SMB2_OPLOCK_LEVEL_II).result()
+
+        bytes_written = chan.write(file,
+                                   0,
+                                   None)
+        self.assertEqual(bytes_written, 0)
+
+        # We should not receive an oplock break
+        with self.assertRaises(pike.model.TimeoutError):
+            file2.oplock_future.wait(timeout=0.1)
 
         chan.close(file)
+        chan.close(file2)
+
+    # Test that 0-byte write does not cause a lease break
+    @pike.test.RequireDialect(pike.smb2.DIALECT_SMB2_1)
+    @pike.test.RequireCapabilities(pike.smb2.SMB2_GLOBAL_CAP_LEASING)
+    def test_write_none_lease(self):
+        chan, tree = self.tree_connect()
+        lease1 = array.array('B',map(random.randint, [0]*16, [255]*16))
+
+        share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
+
+        file = chan.create(tree,
+                           'write.txt',
+                           access=pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE,
+                           share=share_all,
+                           disposition=pike.smb2.FILE_SUPERSEDE,
+                           oplock_level=pike.smb2.SMB2_OPLOCK_LEVEL_NONE).result()
+
+        file2 = chan.create(tree,
+                            'write.txt',
+                            access=pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE,
+                            share=share_all,
+                            disposition=pike.smb2.FILE_SUPERSEDE,
+                            oplock_level=pike.smb2.SMB2_OPLOCK_LEVEL_LEASE,
+                            lease_key=lease1,
+                            lease_state=pike.smb2.SMB2_LEASE_READ_CACHING).result()
+
+        bytes_written = chan.write(file, 64, None)
+        self.assertEqual(bytes_written, 0)
+
+        # We should not receive a lease break
+        with self.assertRaises(pike.model.TimeoutError):
+            file2.lease.future.wait(timeout=0.1)
+
+        chan.close(file)
+        chan.close(file2)
