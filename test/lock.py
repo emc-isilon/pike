59c58f02f0071438344a44d5667b0b12922f37f6
Brian Koropoff <brian.koropoff@isilon.com>
2013-09-18 17:28:45 -0700

    test:lock: add tests of write denial behavior for locks

--- test/lock.py
+++ test/lock.py
@@ -105,3 +105,87 @@ class LockTest(pike.test.PikeTest):
         
         chan.close(file1)
         chan.close(file2)
+
+    def test_deny_write(self):
+        chan, tree = self.tree_connect()
+        buffer = "0123456789012345678901"
+        lock_offset = 8
+        lock_size = 8
+        locks = [(lock_offset, lock_size, pike.smb2.SMB2_LOCKFLAG_EXCLUSIVE_LOCK)]
+
+        share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
+        access = pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE
+
+        # Create file, lock
+        file1 = chan.create(tree,
+                            'lock.txt',
+                            access=access,
+                            share=share_all,
+                            disposition=pike.smb2.FILE_SUPERSEDE).result()
+       
+        bytes_written = chan.write(file1, 0, buffer)
+        self.assertEqual(bytes_written, len(buffer))
+
+        chan.lock(file1, locks).result()
+
+        # Open file again (with delete on close)
+        file2 = chan.create(tree,
+                            'lock.txt',
+                            access=access,
+                            share=share_all,
+                            disposition=pike.smb2.FILE_OPEN,
+                            options=pike.smb2.FILE_DELETE_ON_CLOSE).result()
+
+        cases = ((offset,size) for offset in xrange(0, 16) for size in xrange(0, 16))
+
+        for (offset,size) in cases:
+            if ranges_intersect(offset, offset+size, lock_offset, lock_offset + lock_size):
+                with self.assert_error(pike.ntstatus.STATUS_FILE_LOCK_CONFLICT):
+                    chan.write(file2, offset, 'a' * size)
+
+        chan.close(file1)
+        chan.close(file2)
+
+    def test_allow_zero_byte_write(self):
+        chan, tree = self.tree_connect()
+        buffer = "0123456789012345678901"
+        lock_offset = 8
+        lock_size = 8
+        locks = [(lock_offset, lock_size, pike.smb2.SMB2_LOCKFLAG_EXCLUSIVE_LOCK)]
+
+        share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
+        access = pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE
+
+        # Create file, lock
+        file1 = chan.create(tree,
+                            'lock.txt',
+                            access=access,
+                            share=share_all,
+                            disposition=pike.smb2.FILE_SUPERSEDE).result()
+       
+        bytes_written = chan.write(file1, 0, buffer)
+        self.assertEqual(bytes_written, len(buffer))
+
+        chan.lock(file1, locks).result()
+
+        # Open file again (with delete on close)
+        file2 = chan.create(tree,
+                            'lock.txt',
+                            access=access,
+                            share=share_all,
+                            disposition=pike.smb2.FILE_OPEN,
+                            options=pike.smb2.FILE_DELETE_ON_CLOSE).result()
+
+        chan.write(file2, lock_offset + 1, None)
+
+        chan.close(file1)
+        chan.close(file2)
+
+def range_contains(a1, a2, b):
+    return b >= a1 and b < a2
+
+def ranges_intersect(a1, a2, b1, b2):
+    return a2 > a1 and b2 > b1 and \
+        (range_contains(a1, a2, b1) or range_contains(a1, a2, b2-1) or \
+         range_contains(b1, b2, a1) or range_contains(b1, b2, a2-1))
+            
