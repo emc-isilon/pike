d7a59f00bbb81d876799773cf0f20470d4501962
Masen Furer <mfurer@isilon.com>
2015-11-25 03:07:44 +0000

    abstract common bits of copychunk test cases away

--- test/copychunk.py
+++ test/copychunk.py
@@ -62,49 +62,68 @@ def _gen_test_buffer(length):
 ###
 class TestServerSideCopy(pike.test.PikeTest):
 
-    def _create_and_write(self, chan, tree, filename, content):
+    def setUp(self):
+        self.chan, self.tree = self.tree_connect()
+
+    def tearDown(self):
+        self.chan.tree_disconnect(self.tree)
+        self.chan.logoff()
+
+    def _create_and_write(self, filename, content):
         """ create / overwrite filename with content """
-        fh1 =     chan.create(tree,
-                              filename,
-                              access=access_rwd,
-                              share=share_all,
-                              disposition=pike.smb2.FILE_SUPERSEDE).result()
+        fh1 = self.chan.create(self.tree,
+                               filename,
+                               access=access_rwd,
+                               share=share_all,
+                               disposition=pike.smb2.FILE_SUPERSEDE).result()
 
-        bytes_written = chan.write(fh1,
-                                   0,
-                                   content)
-        chan.close(fh1)
+        bytes_written = self.chan.write(fh1, 0, content)
+        self.chan.close(fh1)
 
-    def _open_src_dst(self, chan, tree, src_filename, dst_filename,
-                      src_disp=pike.smb2.FILE_OPEN,
-                      dst_disp=pike.smb2.FILE_SUPERSEDE):
+    def _open_src_dst(self, src_filename, dst_filename,
+                      src_access=None,
+                      src_disp=None,
+                      src_options=None,
+                      dst_access=None,
+                      dst_disp=None,
+                      dst_options=None):
+        if src_access is None:
+            src_access = pike.smb2.FILE_READ_DATA | pike.smb2.DELETE
+        if dst_access is None:
+            dst_access = access_rwd
+        if src_disp is None:
+            src_disp = pike.smb2.FILE_OPEN
+        if dst_disp is None:
+            dst_disp = pike.smb2.FILE_SUPERSEDE
+        if src_options is None:
+            src_options = pike.smb2.FILE_DELETE_ON_CLOSE
+        if dst_options is None:
+            dst_options = pike.smb2.FILE_DELETE_ON_CLOSE
 
-        src_options = dst_options = pike.smb2.FILE_DELETE_ON_CLOSE
         if src_filename == dst_filename:
             src_options = 0     # if we're opening the same file, only delete one
-        fh_src = chan.create(tree,
-                             src_filename,
-                             access=access_rwd,
-                             share=share_all,
-                             disposition=src_disp,
-                             options=src_options).result()
+        fh_src = self.chan.create(self.tree,
+                                  src_filename,
+                                  access=src_access,
+                                  share=share_all,
+                                  disposition=src_disp,
+                                  options=src_options).result()
 
-        fh_dst = chan.create(tree,
-                             dst_filename,
-                             access=access_rwd,
-                             share=share_all,
-                             disposition=dst_disp,
-                             options=dst_options).result()
+        fh_dst = self.chan.create(self.tree,
+                                  dst_filename,
+                                  access=dst_access,
+                                  share=share_all,
+                                  disposition=dst_disp,
+                                  options=dst_options).result()
         return (fh_src, fh_dst)
 
     def generic_ssc_test_case(self, block, number_of_chunks, total_offset=0):
         """
         copy block in number_of_chunks, offset the destination copy by total_offset
         """
-        chan, tree = self.tree_connect()
         src_filename = "src_copy_chunk_offset.txt"
         dst_filename = "dst_copy_chunk_offset.txt"
-        self._create_and_write(chan, tree, src_filename, block)
+        self._create_and_write(src_filename, block)
 
         total_len = len(block)
         chunk_sz = (total_len / number_of_chunks) + 1
@@ -120,17 +139,20 @@ class TestServerSideCopy(pike.test.PikeTest):
             chunks.append((offset, offset+total_offset, length))
             this_offset += chunk_sz
 
-        fh_src, fh_dst = self._open_src_dst(chan, tree, src_filename, dst_filename)
+        fh_src, fh_dst = self._open_src_dst(src_filename, dst_filename)
 
-        result = chan.copychunk(fh_src, fh_dst, chunks)
+        result = self.chan.copychunk(fh_src, fh_dst, chunks)
         self.assertEqual(result[0][0].chunks_written, number_of_chunks)
         self.assertEqual(result[0][0].total_bytes_written, total_len)
 
         # read each file and verify the result
-        src_buf = chan.read(fh_src, total_len, 0).tostring()
-        self.assertEqual(src_buf, block)
-        dst_buf = chan.read(fh_dst, total_len, total_offset).tostring()
-        self.assertEqual(dst_buf, block)
+        src_buf = self.chan.read(fh_src, total_len, 0).tostring()
+        self.assertBufferEqual(src_buf, block)
+        dst_buf = self.chan.read(fh_dst, total_len, total_offset).tostring()
+        self.assertBufferEqual(dst_buf, block)
+
+        self.chan.close(fh_src)
+        self.chan.close(fh_dst)
 
     def test_copy_small_file(self):
         block = "Hello"
@@ -175,9 +197,8 @@ class TestServerSideCopy(pike.test.PikeTest):
         duplicate block in number_of_chunks to the same file,
         the copy will be total_offset from the current end of file
         """
-        chan, tree = self.tree_connect()
         filename = "src_copy_chunk_same.txt"
-        self._create_and_write(chan, tree, filename, block)
+        self._create_and_write(filename, block)
 
         total_len = len(block)
         chunk_sz = (total_len / number_of_chunks) + 1
@@ -193,19 +214,24 @@ class TestServerSideCopy(pike.test.PikeTest):
             chunks.append((offset, offset+total_len+total_offset, length))
             this_offset += chunk_sz
 
-        fh_src, fh_dst = self._open_src_dst(chan, tree, filename, filename,
+        fh_src, fh_dst = self._open_src_dst(filename, filename,
                                             dst_disp=pike.smb2.FILE_OPEN_IF)
 
-        result = chan.copychunk(fh_src, fh_dst, chunks)
+        result = self.chan.copychunk(fh_src, fh_dst, chunks)
         self.assertEqual(result[0][0].chunks_written, number_of_chunks)
         self.assertEqual(result[0][0].total_bytes_written, total_len)
 
         # read the file and verify the result
-        src_buf = chan.read(fh_src, total_len*2+total_offset, 0).tostring()
-        self.assertEqual(src_buf[:total_len], block)
-        self.assertEqual(src_buf[total_len:total_len+total_offset],
-                         "\x00"*total_offset)
-        self.assertEqual(src_buf[total_len+total_offset:], block)
+        src_buf = self.chan.read(fh_src, total_len, 0).tostring()
+        self.assertBufferEqual(src_buf, block)
+        if total_offset > 0:
+            offs_buf = self.chan.read(fh_src, total_offset, total_len).tostring()
+            self.assertBufferEqual(offs_buf, "\x00"*total_offset)
+        dst_buf = self.chan.read(fh_src, total_len, total_len+total_offset).tostring()
+        self.assertBufferEqual(dst_buf, block)
+
+        self.chan.close(fh_src)
+        self.chan.close(fh_dst)
 
     def test_same_small_file(self):
         block = "Hello"
@@ -245,10 +271,9 @@ class TestServerSideCopy(pike.test.PikeTest):
         copy block in number_of_chunks, each destination block offset will be overlapped
         over the previous block by overlap_each_block bytes
         """
-        chan, tree = self.tree_connect()
         src_filename = "src_copy_chunk_overlap.txt"
         dst_filename = "dst_copy_chunk_overlap.txt"
-        self._create_and_write(chan, tree, src_filename, block)
+        self._create_and_write(src_filename, block)
 
         total_len = len(block)
         chunk_sz = (total_len / number_of_chunks) + 1
@@ -269,23 +294,23 @@ class TestServerSideCopy(pike.test.PikeTest):
             dst_block[dst_offset:dst_offset+length] = \
                     src_block[offset:offset+length]
             this_offset += chunk_sz
-        if len(chunks) == 14:
-            print(chunk_sz)
-            print(total_len)
-            print(chunks)
+        dst_len = dst_offset+length
+        dst_block = "".join(dst_block[:dst_len])
 
-        fh_src, fh_dst = self._open_src_dst(chan, tree, src_filename, dst_filename)
+        fh_src, fh_dst = self._open_src_dst(src_filename, dst_filename)
 
-        result = chan.copychunk(fh_src, fh_dst, chunks)
+        result = self.chan.copychunk(fh_src, fh_dst, chunks)
         self.assertEqual(result[0][0].chunks_written, number_of_chunks)
         self.assertEqual(result[0][0].total_bytes_written, total_len)
 
         # read each file and verify the result
-        src_buf = chan.read(fh_src, total_len, 0).tostring()
-        self.assertEqual(src_buf, block)
-        dst_len = chunks[-1][1]+chunks[-1][2]
-        dst_buf = chan.read(fh_dst, dst_len, 0).tostring()
-        self.assertEqual(dst_buf, "".join(dst_block[:dst_len]))
+        src_buf = self.chan.read(fh_src, total_len, 0).tostring()
+        self.assertBufferEqual(src_buf, block)
+        dst_buf = self.chan.read(fh_dst, dst_len, 0).tostring()
+        self.assertBufferEqual(dst_buf, dst_block)
+
+        self.chan.close(fh_src)
+        self.chan.close(fh_dst)
 
     def test_overlap_multiple_chunks(self):
         block = _gen_test_buffer(65535)
