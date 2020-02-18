299d1fb8c538104ccbac8f153a2731d1743930b2
Masen Furer <mfurer@isilon.com>
2020-02-18 14:33:45 -0800

    setup.py: remove py2.6 hack on win32

--- setup.py
+++ setup.py
@@ -14,20 +14,7 @@ from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatfo
 
 # attempt building the kerberos extension
 try_krb = True
-
-if sys.platform == "win32" and sys.version_info > (2, 6):
-    # 2.6's distutils.msvc9compiler can raise an IOError when failing to
-    # find the compiler
-    # It can also raise ValueError http://bugs.python.org/issue7511
-    ext_errors = (
-        CCompilerError,
-        DistutilsExecError,
-        DistutilsPlatformError,
-        IOError,
-        ValueError,
-    )
-else:
-    ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
+ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
 
 
 class BuildFailed(Exception):
