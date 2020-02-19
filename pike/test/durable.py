9a4592e7349bd4ad313f4f77ee1f6213d5fc97c9
Masen Furer <mfurer@isilon.com>
2020-02-19 14:08:23 -0800

    pike.test.durable: remove list-map cast

--- pike/test/durable.py
+++ pike/test/durable.py
@@ -53,16 +53,16 @@ class InvalidNetworkResiliencyRequestRequest(pike.smb2.NetworkResiliencyRequestR
 @pike.test.RequireCapabilities(pike.smb2.SMB2_GLOBAL_CAP_LEASING)
 class DurableHandleTest(pike.test.PikeTest):
     share_all = pike.smb2.FILE_SHARE_READ | pike.smb2.FILE_SHARE_WRITE | pike.smb2.FILE_SHARE_DELETE
-    lease1 = array.array('B',list(map(random.randint, [0]*16, [255]*16)))
-    lease2 = array.array('B',list(map(random.randint, [0]*16, [255]*16)))
+    lease1 = array.array('B', map(random.randint, [0] * 16, [255] * 16))
+    lease2 = array.array('B', map(random.randint, [0] * 16, [255] * 16))
     r = pike.smb2.SMB2_LEASE_READ_CACHING
     rw = r | pike.smb2.SMB2_LEASE_WRITE_CACHING
     rh = r | pike.smb2.SMB2_LEASE_HANDLE_CACHING
     rwh = rw | rh
     buffer_too_small_error = pike.ntstatus.STATUS_INVALID_PARAMETER
 
-
-    def create(self, chan, tree, durable, lease=rwh, lease_key=lease1, disposition=pike.smb2.FILE_SUPERSEDE):
+    def create(self, chan, tree, durable, lease=rwh, lease_key=lease1,
+               disposition=pike.smb2.FILE_SUPERSEDE):
         return chan.create(tree,
                            'durable.txt',
                            access=pike.smb2.FILE_READ_DATA | pike.smb2.FILE_WRITE_DATA | pike.smb2.DELETE,
