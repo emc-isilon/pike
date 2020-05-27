acb879aac1455f2926cdfcd524919f85961f365f
Arnab Das <arnab.das@emc.com>
2020-05-27 11:33:13 -0700

    Set required client min & max dialect before test

--- pike/test/__init__.py
+++ pike/test/__init__.py
@@ -383,15 +383,11 @@ class PikeTest(unittest.TestCase):
         unittest.TestCase.__init__(self, *args, **kwargs)
         self.init_once()
         self._connections = []
-        self.default_client = default_client()
-        self.server = Options.server()
-        self.port = Options.port()
-        self.creds = Options.creds()
-        self.share = Options.share()
-        self.signing = Options.signing()
-        self.encryption = Options.encryption()
-        self.min_dialect = Options.min_dialect()
-        self.max_dialect = Options.max_dialect()
+        self.default_client = model.Client()
+        self.set_client_dialect(self.min_dialect, self.max_dialect)
+        if self.signing:
+            self.default_client.security_mode = (smb2.SMB2_NEGOTIATE_SIGNING_ENABLED |
+                                                 smb2.SMB2_NEGOTIATE_SIGNING_REQUIRED)
 
     def debug(self, *args, **kwargs):
         self.logger.debug(*args, **kwargs)
@@ -407,6 +403,19 @@ class PikeTest(unittest.TestCase):
 
     def critical(self, *args, **kwargs):
         self.logger.critical(*args, **kwargs)
+
+    def set_client_dialect(self, min_dialect=None, max_dialect=None,
+                           client=None):
+        if client is None:
+            client = self.default_client
+        if (min_dialect is not None and
+			min(client.dialects) != min_dialect):
+			client.dialects = [d for d in client.dialects
+                if d >= self.min_dialect]
+        if (max_dialect is not None and
+			max(client.dialects) != max_dialect):
+			client.dialects = [d for d in client.dialects
+                if d <= self.max_dialect]
 
     def tree_connect(self, client=None, resume=None):
         tc = TreeConnect(
