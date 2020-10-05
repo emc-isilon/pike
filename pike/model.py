07d6ff332c652cff1d15e724f395146d89c33236
Masen Furer <mfurer@isilon.com>
2020-10-05 15:53:48 -0700

    pike.model: extend TimeoutError
    TimeoutError.with_future is a factory classmethod that returns a TimeoutError
    with the .future attribute set.
    
    TimeoutError.__str__ formats the exception with the Future.request and
    Future.interim_response (if they exist).

--- pike/model.py
+++ pike/model.py
@@ -88,7 +88,38 @@ def loop(timeout=None, count=None):
     transport.loop(timeout=timeout, count=count)
 
 class TimeoutError(Exception):
-    pass
+    """Future completion timed out"""
+    future = None
+
+    @classmethod
+    def with_future(cls, future, *args):
+        """
+        Instantiate TimeoutError from a given future.
+
+        :param future: Future that timed out
+        :param args: passed to Exception.__init__
+        :return: TimeoutError
+        """
+        ex = cls(*args)
+        ex.future = future
+        return ex
+
+    def __str__(self):
+        s = super(TimeoutError, self).__str__()
+        if self.future is not None:
+            if self.future.request is not None:
+                requests = [str(self.future.request)]
+                if not isinstance(self.future.request, (core.Frame, str, bytes)):
+                    # attempt to recursively str format other iterables
+                    try:
+                        requests = [str(r) for r in self.future.request]
+                    except TypeError:
+                        pass
+                s += "\nRequest: {}".format("\n".join(requests))
+            if self.future.interim_response is not None:
+                s += "\nInterim: {}".format(self.future.interim_response)
+        return s
+
 
 class StateError(Exception):
     pass
