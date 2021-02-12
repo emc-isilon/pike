46cbeaacd5d5f4b69888ff96509f76060fa33a56
Masen Furer <mfurer@isilon.com>
2021-02-12 07:47:33 -0800

    test_path: relative_to and parents are working now
    simplifying the PikePath implementation allows existing relative_to and
    parents methods to work without further changes.
    
    add a test for join_from_root

--- pike/test/test_path.py
+++ pike/test/test_path.py
@@ -1,10 +1,14 @@
 """
 Tests for pike path-like interface
 """
+import os
 import uuid
 
 import pytest
 
+import pike.model as model
+import pike.ntstatus as ntstatus
+from pike.path import PikePath
 import pike.smb2 as smb2
 
 
@@ -14,7 +18,7 @@ ACCESS_RWD = smb2.GENERIC_READ | smb2.GENERIC_WRITE | smb2.DELETE
 @pytest.fixture
 def test_path(pike_TreeConnect):
     with pike_TreeConnect() as tc:
-        test_root = tc.tree.pathlike / "test_{}".format(uuid.uuid4())
+        test_root = tc.tree / "test_{}".format(uuid.uuid4())
         test_root.mkdir()
         yield test_root
 
@@ -27,20 +31,24 @@ def test_exists(test_path):
     unique_id = str(uuid.uuid4())
     fname = test_path / unique_id
     assert not fname.exists()
-    with test_path._channel.create(test_path._tree, fname._path, access=ACCESS_RWD, options=smb2.FILE_DELETE_ON_CLOSE).result() as fh:
+    with test_path._channel.create(
+        test_path._tree,
+        fname._path,
+        access=ACCESS_RWD,
+        options=smb2.FILE_DELETE_ON_CLOSE,
+    ).result() as fh:
         assert fname.exists()
     assert not fname.exists()
 
+
 def test_iterdir(test_path):
-    filedata = {
-        name: str(uuid.uuid4())
-        for name in ["f1", "f2", "f3", "f4"]
-    }
+    filedata = {name: str(uuid.uuid4()) for name in ["f1", "f2", "f3", "f4"]}
     for f, data in filedata.items():
         (test_path / f).write_text(data)
     for fpth in test_path.iterdir():
         assert fpth.name in filedata
         assert fpth.read_text() == filedata[fpth.name]
+
 
 def test_mkdir(test_path):
     pth = test_path / "foo" / "bar" / "baz"
@@ -49,11 +57,13 @@ def test_mkdir(test_path):
         pth.rmdir()
         pth = pth.parent
 
+
 def test_write_read(test_path):
     test_bytes = b"hola pika" * (1024 * 1024)
     pth = test_path / "bar.txt"
     pth.write_bytes(test_bytes)
     assert pth.read_bytes() == test_bytes
+
 
 def test_open(test_path):
     pth = test_path / "foo.txt"
@@ -62,3 +72,29 @@ def test_open(test_path):
     f.write("foo\n\n")
     f.close()
     assert pth.read_bytes() == b"this is a test test test\nfoo\n\n"
+
+
+def test_relative_to(test_path):
+    pth = test_path / "foo"
+    assert str(pth.relative_to(test_path)) == "foo"
+
+
+def test_parents(test_path):
+    pcs = ["foo", "bar", "baz"]
+    pth = test_path.joinpath(*pcs)
+    parents = pth.parents
+    for ix, pc in enumerate(reversed(pcs[:-1])):
+        print(parents[ix])
+        print(parents[ix].exists())
+        assert parents[ix].name == pc
+
+
+def test_join_from_root(test_path, pike_TreeConnect):
+    with pike_TreeConnect() as tc2:
+        pth = test_path / "rehome.txt"
+        with pth.open("w") as f:
+            f.write("foo the bar\n")
+            # now attempt to open the file from the second connection
+            with model.pike_status(ntstatus.STATUS_SHARING_VIOLATION):
+                from_root = PikePath(tc2.tree).join_from_root(pth)
+                print(from_root.read_text())
