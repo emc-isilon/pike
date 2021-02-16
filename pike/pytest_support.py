ea6d157726283b4992c7e2e88d424ce5f93cd4d4
Masen Furer <mfurer@isilon.com>
2021-02-16 14:11:58 -0800

    pike.pytest_support: docstrings
    add docstrings describing the fixtures and markers

--- pike/pytest_support.py
+++ pike/pytest_support.py
@@ -3,8 +3,25 @@
 # All rights reserved.
 # See file LICENSE for licensing information.
 #
+"""
+Pike/pytest integration
 
-"""Pike/pytest integration."""
+Available markers:
+
+    - ``require_dialect(min_dialect, max_dialect)``: raise a
+      :py:class:`~pike.test.DialectMissing` exception if the target server does
+      not support a dialect in the given range. This exception will be raised
+      after Negotiate.
+    - ``require_capability(global_capabilities)``: raise a
+      :py:class:`~pike.test.CapabilityMissing` exception if the target server
+      does not support all of the OR'd global capability flags.  This exception
+      will be raised after Negotiate.
+    - ``require_share_capability(share_capabilities)``: raise a
+      :py:class:`~pike.test.ShareCapabilityMissing` exception if the target
+      share does not support all of the OR'd share capability flags.  This
+      exception will be raised after Tree Connect.
+"""
+
 import uuid
 import time
 
@@ -58,6 +75,20 @@ def get_mark_value(item, mark_name, default):
 
 @pytest.fixture
 def pike_TreeConnect(request):
+    """
+    Function-scope fixture returns a wrapper function around
+    :py:class:`pike.TreeConnect`.
+
+    The wrapper-function must be called to establish the connection and the
+    returned object may be used as a contextmanager.
+
+    The wrapper-function will pass through any ``require_dialect``,
+    ``require_capabilities``, and ``require_share_capabilities`` marks on the
+    test item and raise an exception if the target server doesn't support the
+    required values.
+
+    :return: Callable[..., pike.TreeConnect]
+    """
     marks = (
         ("require_dialect", (0, float("inf"))),
         ("require_capabilities", 0),
@@ -76,11 +107,24 @@ def pike_TreeConnect(request):
             kwargs["require_dialect"] = (kwargs["require_dialect"], float("inf"))
         return pike.test.TreeConnect(*args, **kwargs)
 
+    # carry the docstring to the wrapper function
+    TreeConnect.__doc__ = pike.TreeConnect.__doc__
+
     return TreeConnect
 
 
 @pytest.fixture
 def pike_tmp_path(pike_TreeConnect):
+    """
+    Function-scope fixture reaturns a :py:class:`~pike.path.PikePath` rooted at
+    a timestamped subdirectory of the share.
+
+    The temporary directory is not deleted during tearDown and should be
+    removed by the test case itself.
+
+    A function's :py:func:`pike_TreeConnect` fixture will access the same
+    share, however, via a separate SMB2 session.
+    """
     with pike_TreeConnect() as tc:
         test_root = tc / "pike_{}_{}".format(time.strftime("%Y-%m-%d_%H%M%S"), str(uuid.uuid4())[:5])
         test_root.mkdir()
