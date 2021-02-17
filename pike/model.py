2e6b819bb7a8eeb732a859147c7b42f8ace31717
Masen Furer <mfurer@isilon.com>
2021-02-16 18:15:56 -0800

    pike.model: import exceptions from pike.exceptions
    avoid circular import issues

--- pike/model.py
+++ pike/model.py
@@ -55,6 +55,14 @@ from . import smb2
 from . import transport
 from . import ntstatus
 from . import digest
+from .exceptions import (
+    CallbackError,
+    CreditError,
+    RequestError,
+    ResponseError,
+    StateError,
+    TimeoutError,
+)
 import pike.io
 
 default_credit_request = 10
@@ -73,68 +81,6 @@ def loop(timeout=None, count=None):
         timeout = default_timeout
     transport.loop(timeout=timeout, count=count)
 
-
-class TimeoutError(Exception):
-    """Future completion timed out"""
-
-    future = None
-
-    @classmethod
-    def with_future(cls, future, *args):
-        """
-        Instantiate TimeoutError from a given future.
-
-        :param future: Future that timed out
-        :param args: passed to Exception.__init__
-        :return: TimeoutError
-        """
-        ex = cls(*args)
-        ex.future = future
-        return ex
-
-    def __str__(self):
-        s = super(TimeoutError, self).__str__()
-        if self.future is not None:
-            if self.future.request is not None:
-                requests = [str(self.future.request)]
-                if not isinstance(self.future.request, (core.Frame, str, bytes)):
-                    # attempt to recursively str format other iterables
-                    try:
-                        requests = [str(r) for r in self.future.request]
-                    except TypeError:
-                        pass
-                s += "\nRequest: {}".format("\n".join(requests))
-            if self.future.interim_response is not None:
-                s += "\nInterim: {}".format(self.future.interim_response)
-        return s
-
-
-class StateError(Exception):
-    pass
-
-
-class CreditError(Exception):
-    pass
-
-
-class RequestError(Exception):
-    def __init__(self, request, message=None):
-        if message is None:
-            message = "Could not send {0}".format(repr(request))
-        Exception.__init__(self, message)
-        self.request = request
-
-
-class CallbackError(Exception):
-    """
-    the callback was not suitable
-    """
-
-
-class ResponseError(Exception):
-    def __init__(self, response):
-        Exception.__init__(self, response.command, response.status)
-        self.response = response
 
 
 class AssertNtstatusContext(object):
