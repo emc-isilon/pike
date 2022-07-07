c9883a61871b89e1e17bda76d92fbf11eacb696a
Masen Furer <mfurer@isilon.com>
2022-07-07 23:48:05 +0000

    build_all_wheels.sh: simplify build script
    install `build` in the user area
    
    include instructions to install `twine` and activate it, but don't do it
    for the user

--- build_all_wheels.sh
+++ build_all_wheels.sh
@@ -4,10 +4,6 @@
 
 # ensure system build requirements are met (see README.md)
 
-# any additional arguments supplied will be passed to the final
-# `setup.py bdist_wheel` command and may be used to upload the
-# packages to artifactory
-
 # recommend the use of `pyenv` to install and activate
 # multiple python versions before running the script.
 
@@ -15,13 +11,14 @@ set -euo pipefail
 
 versions=(python2 python3.6 python3.7 python3.8 python3.9 python3.10)
 
-python3.10 -m pip install twine
-rm -rf .build dist
+rm -rf dist
 for v in ${versions[@]}
 do
-    $v -m pip install -U pip && \
-    $v -m pip install 'build[virtualenv]' && \
-    $v -m build
+    $v -m pip install --user 'build[virtualenv]' && $v -m build
 done
 
-echo 'To upload run: python3.10 -m twine upload dist/* --repository-url "<my-private-repo>"'
+cat <<EOF
+To upload packages run:
+python3 -m pip install --user twine
+python3 -m twine upload dist/* --repository-url "<my-private-repo>"'
+EOF
