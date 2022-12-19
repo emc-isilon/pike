375dc4248b41f4734c8f9a57e736c34754b92cb8
Masen Furer <m_github@0x26.net>
2022-12-19 13:52:57 -0800

    Move static package metadata to setup.cfg
    Remove most custom logic from setup.py
      - if libkrb5 is available, attempt to build it, otherwise build
        without c extension
    
    To retain py27 build compatibility, use setup.cfg
    to convey static metadata, rather than PEP 621 (TODO)

--- setup.py
+++ setup.py
@@ -4,53 +4,16 @@
 # All rights reserved.
 # See file LICENSE for licensing information.
 #
-
 import ctypes
 import os
-import unittest
 
 try:
-    from setuptools import setup, Extension, Command
+    from setuptools import setup, Extension
 except ImportError:
-    from distutils.core import setup, Extension, Command
-from distutils.command.build_ext import build_ext
-from distutils.command.build_py import build_py
+    from distutils.core import setup, Extension
 from distutils.errors import CCompilerError, DistutilsExecError, DistutilsPlatformError
 
 _HERED = os.path.abspath(os.path.dirname(__file__))
-_README = os.path.join(_HERED, "README.md")
-
-# attempt building the kerberos extension
-try_krb = True
-ext_errors = (CCompilerError, DistutilsExecError, DistutilsPlatformError)
-
-
-class BuildFailed(Exception):
-    pass
-
-
-class ve_build_ext(build_ext):
-    # This class allows C extension building to fail.
-
-    def run(self):
-        try:
-            build_ext.run(self)
-        except DistutilsPlatformError:
-            raise BuildFailed()
-
-    def build_extension(self, ext):
-        try:
-            build_ext.build_extension(self, ext)
-        except ext_errors:
-            raise BuildFailed()
-
-
-class ve_build_py(build_py):
-    def run(self, *args, **kwds):
-        if not try_krb:
-            print("libgssapi_krb5 not available, skipping kerberos module")
-        build_py.run(self, *args, **kwds)
-
 
 try:
     libgssapi_krb5 = ctypes.CDLL("libgssapi_krb5.so")
@@ -74,83 +37,7 @@ try:
         libraries=["gssapi_krb5"],
         define_macros=defines,
     )
-except OSError:
-    try_krb = False
-
-
-def pike_suite():
-    return unittest.defaultTestLoader.discover("pike/test", pattern="*.py")
-
-
-# Get the long description from the README.md file
-with open(_README, "rb") as f_:
-    long_description = f_.read().decode("utf-8")
-
-
-def run_setup(with_extensions):
-    ext_modules = []
-    cmdclass = {"build_py": ve_build_py}
-    if with_extensions:
-        ext_modules.append(lw_krb_module)
-        cmdclass = dict(cmdclass, build_ext=ve_build_ext)
-    setup(
-        name="pike-smb2",
-        use_scm_version=True,
-        description="Pure python SMB client",
-        long_description_content_type="text/markdown",
-        long_description=long_description,
-        author="Brian Koropoff",
-        author_email="Brian.Koropoff@emc.com",
-        maintainer="Masen Furer",
-        maintainer_email="Masen.Furer@dell.com",
-        url="https://github.com/emc-isilon/pike",
-        project_urls={
-            "Source": "https://github.com/emc-isilon/pike",
-            "Bug Reports": "https://github.com/emc-isilon/pike/issues",
-        },
-        license="Simplified BSD License",
-        packages=["pike", "pike.test"],
-        entry_points={
-            "pytest11": [
-                "pike = pike.pytest_support",
-            ]
-        },
-        python_requires=">=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*,!=3.4.*,!=3.5.*",
-        install_requires=[
-            'enum34~=1.1.6;  python_version ~= "2.7"',
-            "attrs >= 19.3",
-            'pathlib2~=2.3.5;  python_version ~= "2.7"',
-            "pycryptodome",
-            "future",
-            "six",
-        ],
-        ext_modules=ext_modules,
-        test_suite="setup.pike_suite",
-        cmdclass=cmdclass,
-        # see https://pypi.org/classifiers/
-        classifiers=[
-            "Development Status :: 4 - Beta",
-            "Intended Audience :: Developers",
-            "Topic :: Software Development :: Testing",
-            "Programming Language :: Python",
-            "Programming Language :: Python :: 2",
-            "Programming Language :: Python :: 2.7",
-            "Programming Language :: Python :: 3",
-            "Programming Language :: Python :: 3.6",
-            "Programming Language :: Python :: 3.7",
-            "Programming Language :: Python :: 3.8",
-            "Programming Language :: Python :: 3.9",
-            "Programming Language :: Python :: 3.10",
-            "Programming Language :: Python :: Implementation :: CPython",
-            "Operating System :: OS Independent",
-            "Environment :: Console",
-            "License :: OSI Approved :: BSD License",
-        ],
-        keywords="smb smb-testing smb-client",
-    )
-
-
-try:
-    run_setup(with_extensions=try_krb)
-except BuildFailed:
-    run_setup(with_extensions=False)
+    setup(ext_modules=[lw_krb_module])
+except (OSError, CCompilerError, DistutilsExecError, DistutilsPlatformError):
+    print("libgssapi_krb5 not available, skipping kerberos module")
+    setup()
